3758e9c922cf3037cf270a3f798c6ee57a901828
yushicheng <1542874601@qq.com>
2022-06-30 23:26:26 +0800

    temp:临时节点标记 星期四 晚上 11:26:26 六月 30日 2022 修改了 package.json 修改了 src/app.controller.ts 修改了 src/app.module.ts 修改了 src/app.service.ts 修改了 src/guards/user_guard.guard.ts 修改了 src/main.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,18 @@
+import { MessagePattern } from "@nestjs/microservices";
 import { Inject, Controller, Get, CACHE_MANAGER } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
 
-import { CommodityEntity } from "@/providers/commodity_entity.providers";
-import { OrderRecordEntity } from "@/providers/order_record_entity.providers";
-import { TransactionRecordEntity } from "@/providers/transaction_record_entity.providers";
-
-@Controller("/v1/")
+@Controller("/api/v1/")
 export class AppController {
-  constructor(
-    @Inject(CACHE_MANAGER) private redis_cache,
-    @InjectRepository(OrderRecordEntity) private order_record,
-    @InjectRepository(CommodityEntity) private commodity_record,
-    @InjectRepository(TransactionRecordEntity) private transaction_record,
-  ) {}
+  constructor(@Inject(CACHE_MANAGER) private redis_cache) {}
 
   @Get("hello")
   hello() {
     this.redis_cache.set("test", "hello words");
     return "hello words";
+  }
+
+  @MessagePattern("test")
+  async test() {
+    return "this is the test";
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,8 @@ import * as redisStore from "cache-manager-redis-store";
 
 import { AppController } from "@/app.controller";
 import { UserController } from "@/modules/version1/user.controller";
-import { SystemController } from "@/modules/version1/system.controller";
-import { OrdersController } from "@/modules/version1/orders.controller";
-import { CommodityController } from "@/modules/version1/commodity.controller";
 
-import { CommodityEntity } from "@/providers/commodity_entity.providers";
 import { UserAccountEntity } from "@/providers/user_account_entity.providers";
-import { OrderRecordEntity } from "@/providers/order_record_entity.providers";
-import { TransactionRecordEntity } from "@/providers/transaction_record_entity.providers";
 
 import { AuthService } from "@/services/version1/auth.service";
 
@@ -29,37 +23,12 @@ import { AuthService } from "@/services/version1/auth.service";
       host: "localhost",
       username: "root",
       password: "",
-      database: "orders",
-      entities: [
-        CommodityEntity,
-        UserAccountEntity,
-        OrderRecordEntity,
-        TransactionRecordEntity,
-      ],
+      entities: [],
       synchronize: true,
     }),
-    TypeOrmModule.forRoot({
-      port: 3306,
-      type: "mysql",
-      host: "localhost",
-      username: "root",
-      password: "",
-      database: "user",
-      entities: [UserAccountEntity],
-      synchronize: true,
-    }),
-    TypeOrmModule.forFeature([CommodityEntity]),
     TypeOrmModule.forFeature([UserAccountEntity]),
-    TypeOrmModule.forFeature([OrderRecordEntity]),
-    TypeOrmModule.forFeature([TransactionRecordEntity]),
   ],
-  controllers: [
-    AppController,
-    UserController,
-    SystemController,
-    OrdersController,
-    CommodityController,
-  ],
+  controllers: [AppController, UserController],
   providers: [AuthService],
 })
 export class AppModule {}
