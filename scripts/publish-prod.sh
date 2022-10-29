122fad73512df6d7f57264bf0cbd3c441e46e35d
yushicheng <1542874601@qq.com>
2022-10-29 22:27:41 +0800

    temp:临时节点标记 星期六 晚上 10:27:41 十月 29日 2022 修改了 scripts/publish-prod.sh 修改了 scripts/publish-test.sh

--- scripts/publish-prod.sh
+++ scripts/publish-prod.sh
@@ -6,5 +6,6 @@ cd $PROJECT_PATH;
 IMAGE_TAG="example-service-prod";
 REMOTE_NAME="18058173171/example-service-prod:latest";
 
+docker rmi $REMOTE_NAME --force;
 docker tag $IMAGE_TAG $REMOTE_NAME;
 docker push $REMOTE_NAME;
--- scripts/publish-test.sh
+++ scripts/publish-test.sh
@@ -6,5 +6,6 @@ cd $PROJECT_PATH;
 IMAGE_TAG="example-service-test";
 REMOTE_NAME="18058173171/example-service-test:latest";
 
+docker rmi $REMOTE_NAME --force;
 docker tag $IMAGE_TAG $REMOTE_NAME;
 docker push $REMOTE_NAME;
